d8e2c7673db61a1b6ca17edd0dcd8a0fe38ad392
Kyle Coble <kwc57@cornell.edu>
2023-03-21 20:30:11 -0400

    Try using default port

--- ci_test.sh
+++ ci_test.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 
 cargo build
-cargo run --example amiga-mock-server -- --port 50050 &
+cargo run --example amiga-mock-server -- --port 50060 &
 sleep 2
-roslaunch launch/amiga_ros_bridge.launch port:=50050 host:=[::1] &
+roslaunch launch/amiga_ros_bridge.launch host:=[::1] &
 sleep 2
 cargo run --example test_amiga_vel_pubsub
 echo DONE
