b49d6602ec8dfa484a406bc0bd877ea58b96bd08
Kyle Coble <kwc57@cornell.edu>
2023-03-21 20:10:59 -0400

    Fix CI test

--- ci_test.sh
+++ ci_test.sh
@@ -3,11 +3,9 @@ set -e
 set -x
 
 cargo build
-roslaunch launch/test_launch.launch &
-sleep 2
 cargo run --example amiga-mock-server -- --port 50050 &
 sleep 2
-cargo run -- --port 50050 &
+roslaunch launch/amiga_ros_bridge.launch port:=50050 host:=[::1] &
 sleep 2
 cargo run --example test_amiga_vel_pubsub
 echo DONE
