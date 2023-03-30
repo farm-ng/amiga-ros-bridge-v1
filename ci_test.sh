649dc9ab36888f80b84ceac87e86ec0eba9d64a6
edgar <edgar.riba@gmail.com>
2023-03-30 16:27:15 +0200

    use local launch file

--- ci_test.sh
+++ ci_test.sh
@@ -8,7 +8,7 @@ set -x
 cargo build
 cargo run --example amiga-mock-server -- --port 50050 &
 sleep 5
-roslaunch amiga_ros_bridge amiga_ros_bridge.launch port:=50050 host:=[::1] &
+roslaunch launch/amiga_ros_bridge.launch port:=50050 host:=[::1] &
 sleep 2
 cargo run --example test_amiga_vel_pubsub
 echo DONE
